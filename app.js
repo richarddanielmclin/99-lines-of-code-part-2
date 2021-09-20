df6caf1b659429428d47d36f282a0f47f02acf2d
unknown <richdanmac@gmail.com>
2021-09-20 13:22:44 -0500

    finished

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
-let friends = ["Brandon", "Mel", "LL", "Sophie", "David"];
+document.addEventListener("DOMContentLoaded", function () {
+    let btnDiv = document.createElement("div");
+    let btn = document.createElement("button");    
+    btn.innerHTML = "SING!"
+    btn.className = "btn";
+    btn.id = "btn-id";
+    btnDiv.className = "container"
+    btnDiv.appendChild(btn)
+    document.body.appendChild(btnDiv);
 
-for (let i = 0; i < friends.length; i++) {
-    for (let j = 99; j >= 1; j--) {
-
-        if (j == 2) {
-            console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " line of code in the file")
-
-        } else if (j > 2) {
-            console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file")
-
-        } else {
-            console.log(j + " line of code in the file, " + j + " line of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file")
-
-        }
-
+    document.getElementById(btn.id).onclick = function () {
+        logFriend();
     }
 
-}
+    let friends = ["Brandon", "Mel", "LL", "Sophie", "David"];
+
+    function logFriend() {
+        for (let i = 0; i < friends.length; i++) {
+            let friendDiv = document.createElement("div");
+            friendDiv.className = "friend"
+            let friendName = document.createElement("h3");
+            friendName.innerHTML = friends[i];
+            document.body.appendChild(friendDiv);
+            friendDiv.appendChild(friendName);
+
+            for (let j = 99; j >= 1; j--) {
+                let lyrics = document.createElement("p");
+                friendDiv.appendChild(lyrics);
+
+                if (j == 2) {
+                    lyrics.innerHTML = j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " line of code in the file"
+                } else if (j > 2) {
+                    lyrics.innerHTML = j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file"
+                } else {
+                    lyrics.innerHTML = j + " line of code in the file, " + j + " line of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file!"
+                }
+            }
+        }
+    }
+})
--- readme.md
+++ readme.md
@@ -7,6 +7,7 @@ You should copy your files from part 1 and paste them along with this README fil
 Put a button in the HTML that says Sing!
 
 Stepping Stone: When you click the button, all the console.log statements from the previous lab should happen. They SHOULD NOT happen before the button is clicked.
+
 When you have the button setup and working correctly as described, then:
 Delete the div with class friend and the headings and paragraphs it contains from your html file
 Go to your code where you are "singing the song" and accomplish the following using DOM:
